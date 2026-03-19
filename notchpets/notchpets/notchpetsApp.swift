3e503b7560d5c88fe123aae789093150416cf665
tri27pham <tri.pham2002@outlook.com>
2026-03-19 13:59:57 +0100

    setup: add initial notch expand / shrink

---
+++ notchpets/notchpets/notchpetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchpetsApp.swift
+//  notchpets
+//
+//  Created by Tri on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct notchpetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
